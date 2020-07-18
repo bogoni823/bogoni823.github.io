cf8922a69082d587859c3efbd60d1a51f0e1b0c8
bogoni823 <63904388+bogoni823@users.noreply.github.com>
2020-07-19 00:12:30 +0900

    Delete tooltip.js

--- js/tooltip.js
+++
@@ -1,8 +0,0 @@
-var span_flag = false;
-
-$(document).on("mouseenter", ".btn-3d", function() {
-  if(span_flag) $("#style-span").remove();
-  else span_flag = true;
-  var id = $(this).attr("class").split(" ")[1];
-  $("<style id='style-span'> [data-tooltip-text]:hover:after { left: "+span_left[id]+"; } </style>").appendTo($("head"));
-});
